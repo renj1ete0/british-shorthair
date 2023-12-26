8fba969a2ad193a1f00a91724cf22625faeb45cf
Ren Jie Teo <renjieteo@outlook.com>
2023-12-27 01:33:33 +0800

    Fix typo in file location

--- scripts/create_cronjobs.py
+++ scripts/create_cronjobs.py
@@ -7,13 +7,13 @@ allcronjobs = CronTab(user=config["USER"])
 allcronjobs.remove_all() # Assuming server is only for this project
 
 # Bus Stop Essentials CRON - Bus Svc, Bus Stops, Bus Route JSON (Run at 3am daily)
-job = allcronjobs.new(command='python3 ~/british-shorthair/scripts/target_BS_Essentials.py', comment='BS_Essentials')
+job = allcronjobs.new(command='python3 ~/british-shorthair/scripts/target_BSEssentials.py', comment='BS_Essentials')
 job.hour.on(3)
 job.every_reboot()
 job.run()
 allcronjobs.write()
 
 # Bus Stop Arrival CRON - Bus Arrival JSON (Run every 1min)
-job = allcronjobs.new(command='python3 ~/british-shorthair/scripts/target_BS_Arrival.py', comment='BS_Arrival')
+job = allcronjobs.new(command='python3 ~/british-shorthair/scripts/target_BSArrival.py', comment='BS_Arrival')
 job.minute.every(1)
 allcronjobs.write()
